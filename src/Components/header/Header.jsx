1cdc77be6da6ea1f5354debf5c0c30ff3887c237
Sam <wtawonlife@gmail.com>
2022-06-29 21:54:23 +0900

    🔨 useState를 이용한 Header 버튼 기능 구현

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.css';
 import Logo from '../../assets/Logo.svg';
 import Profile from '../../assets/profile.jpg';
+import LogInBtn from '../../assets/icon-login.svg';
+import RegisterBtn from '../../assets/icon-register.svg';
 import WriteBtn from '../../assets/icon-modify-white.svg';
 import LogOutBtn from '../../assets/icon-logout.svg';
 
@@ -18,7 +20,7 @@ function HeaderLayout(){
     return(
         <div className="max-width">
             <MainLogo/>
-            <LogIn/>
+            <SignIn/>
         </div>
     )
 }
@@ -34,14 +36,74 @@ function MainLogo(){
     );
 }
 
-function LogIn(){
-    return(
-        <ul>
-            <ProfileImg/>
-            <Write/>
-            <LogOut/>
-        </ul>
-    )
+function SignIn(){
+    const [ToggleBtn, setToggleBtn] = useState(false);
+    const handleToggle = function(){
+        setToggleBtn(!ToggleBtn)
+    }
+
+    function LogIn(){
+        return(
+            <li onClick={handleToggle}>
+                <a href="#none" className="button login">
+                <img src={LogInBtn} alt="" />
+                <span>Login</span>
+                </a>
+            </li>
+        )
+    }
+
+    function Write(){
+        return(
+            <li>
+            <a href="#none" className="button">
+            <img src={WriteBtn} alt=""/>
+            <span>Write</span>
+            </a>
+        </li>
+        )
+    }
+
+    function LogOut(){
+        return(
+                <li>
+                <button onClick={handleToggle} className="button white">
+                    <img src={LogOutBtn} alt="" />
+                    <span>Logout</span>
+                </button>
+                </li>
+            )      
+    }
+
+    function Register(){
+        return(
+            <li>
+            <button className="button register">
+                <img src={RegisterBtn} alt="" />
+                <span>Register</span>
+            </button>
+            </li>    
+        )
+    }
+
+    if(ToggleBtn === false){
+        return (
+            <ul>
+                
+                <LogIn/>
+                <Register/>
+            </ul>
+        )
+    }
+    else{
+        return(
+            <ul>
+                <ProfileImg/>
+                <Write/>
+                <LogOut/>
+            </ul>
+        )
+    }
 }
 
 function ProfileImg(){
@@ -54,25 +116,6 @@ function ProfileImg(){
     )
 }
 
-function Write(){
-    return(
-        <li>
-            <a href="#none" className="button">
-            <img src={WriteBtn} alt="" />
-            <span>Write</span>
-            </a>
-        </li>
-    )
-}
 
-function LogOut(){
-   return(
-        <li>
-            <button className="button white">
-                <img src={LogOutBtn} alt="" />
-                <span>Logout</span>
-            </button>
-        </li>
-   )
-}
+
 export default Header;
